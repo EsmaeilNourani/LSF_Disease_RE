90d836c7b88af6d2b5633f128def32dcee7f962f
EsmaeilNourani <esmaeil.nourani@gmail.com>
2024-07-15 15:31:34 +0200

    Delete run_training_pipeline.sh

--- TrainRelationExtractionSystem/run_training_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-### run this code to train four different relation extraction models.
-### each line submits a gpu-job to train a model with a different random_seed
-qsub train_single_re_model.sh 0
-qsub train_single_re_model.sh 1
-qsub train_single_re_model.sh 2
-qsub train_single_re_model.sh 3
